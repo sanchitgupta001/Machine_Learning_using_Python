2d4cfa50b59e7d9daff40f0565696cdd09418ff9
SanchitGupta001 <sanchit.gupta011@gmail.com>
2016-10-08 01:11:20 +0530

    Added SVM.py

---
+++ Classification/SVM.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?',-99999, inplace=True) # There was some missing data denoted by '?' in the dataset.
+# So, here we are replacing '?' by -99999
+df.drop(['id'], 1, inplace=True) # Here we are dropping the 'id' column(No need of this column in classification)
+
+X = np.array(df.drop(['class'],1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
+example_measures = example_measures.reshape(len(example_measures),-1) # More than one prediction can be made
+
+pred = clf.predict(example_measures)
+print(pred)
