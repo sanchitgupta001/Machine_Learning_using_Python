63e2a13d387829921d35390d2c7cd9b8f0894155
SanchitGupta001 <sanchit.gupta011@gmail.com>
2016-10-07 18:29:37 +0530

    Modified KNearest_Neighbour_from_scratch.py to calculate confidence and overall accuracy for some iterations

--- Classification/KNearest_Neighbour_from_scratch.py
+++ Classification/KNearest_Neighbour_from_scratch.py
@@ -26,40 +26,46 @@ def k_nearest_neighbors(data, predict, k=3):
             
     votes = [i[1] for i in sorted(distances)[:k]]        
     vote_result = Counter(votes).most_common(1)[0][0] # Finds the single most common key from votes.
+    confidence = Counter(votes).most_common(1)[0][1]/k # In statistics, a confidence interval (CI) is a type of interval estimate of a population parameter
     # In order to get more than 1 most common pass n required as an argument to most_common() function.
-    return vote_result
+    return vote_result, confidence
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?',-99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-# Some of the data elements may be in quotes(as a string). So, we are converting each element to float datatype.
-full_data = df.astype(float).values.tolist()
-random.shuffle(full_data)
-
-test_size = 0.2
-train_set = {2:[], 4:[]} # 2 : benign and 4 : malignant
-# for more info consult breast-cancer-wisconsin.names file 
-          
-test_set = {2:[],4:[]} 
-train_data = full_data[:-int(test_size*len(full_data))] 
-test_data = full_data[-int(test_size*len(full_data)):]
- 
-# Filling the train_set with train_data
-for i in train_data:
-    train_set[i[-1]].append(i[:-1])  
-
-# Filling the test_set with test_data
-for i in test_data:
-    test_set[i[-1]].append(i[:-1]) 
-
-correct = 0
-total = 0
-
-for group in test_set:
-    for data in test_set[group]:
-        vote = k_nearest_neighbors(train_set, data, k=5)
-        if group == vote:
-            correct += 1
-        total +=1    
+accuracies = []    
+for l in range(10): # Number of times we want to compute accuracies for different datasets
+    df = pd.read_csv('breast-cancer-wisconsin.data')
+    df.replace('?',-99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    # Some of the data elements may be in quotes(as a string). So, we are converting each element to float datatype.
+    full_data = df.astype(float).values.tolist()
+    random.shuffle(full_data)
     
-print('Accuracy : ',correct/total)        
+    test_size = 0.4
+    train_set = {2:[], 4:[]} # 2 : benign and 4 : malignant
+    # for more info consult breast-cancer-wisconsin.names file 
+              
+    test_set = {2:[], 4:[]} 
+    train_data = full_data[:-int(test_size*len(full_data))] 
+    test_data = full_data[-int(test_size*len(full_data)):]
+     
+    # Filling the train_set with train_data
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])  
+    
+    # Filling the test_set with test_data
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1]) 
+    
+    correct = 0
+    total = 0
+    
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+            if group == vote:
+                correct += 1
+            total +=1    
+        
+    # print('Accuracy : ',correct/total)
+    accuracies.append(correct/total)
+
+print(sum(accuracies)/len(accuracies))        
